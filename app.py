84d2a1dadcea001539a2f95fa185f0b1fa48d57f
Nimeshkumar Italiya <=>
2022-07-17 16:44:19 +0530

    test

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
 import logging as logger
+from flask_cors import CORS
 logger.basicConfig(level="DEBUG")
 
 flaskAppInstance = Flask(__name__)
-
+CORS(flaskAppInstance)
 if __name__ == '__main__':
 
     logger.debug("Starting Flask Server")
